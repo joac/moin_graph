1e84955cb0c1d1361b94edf99b3182f9ac2019cd
Joaquín Sorianello <joac@gcoop.coop>
2011-03-25 12:31:33 -0300

    Agrego dump y cambios varios

--- grapher.py
+++ grapher.py
@@ -1,20 +1,23 @@
 #!-*- coding:utf8 -*-
+from __future__ import with_statement
+import pickle
 import random
 import os
 import sys
 import pygraphviz as gv
 from MoinMoin import wikiutil, user
 from MoinMoin.request.request_cli import Request
-from MoinMoin.Page import Page
+from MoinMoin.Page import Page, RootPage
 
 def int2hex_color(number):
+    """Convierte un numero entero en un string de color valido"""
     hexa = hex(number)[2:8]
-    print hexa
     longitud = len(hexa)
     if  longitud < 6:
         hexa = ('0'*(6-longitud))+hexa
-    print hexa  
     return '#'+hexa
+
+
 
 #Agregamos el path al wikiconfig.py de pyar
 sys.path.append('/home/www-pyar/moin/share/moin/pyar')
@@ -27,32 +30,62 @@ ignore =[u'ReadWriteGroup',u'AdminGroup',u'BadContent']
 request = Request()
 
 #Levantamos todas las paginas existentes en el wiki y las convertimos a wikiname
-pages = [wikiutil.unquoteWikiname(page) for page in os.listdir(path)]
-pages = [page for page in pages if Page(request, page).exists()]
+# FIXME hace que las paginas no se levanten del disco y se use la api de moinmoin
 
-#borramos algunas paginas que no deberian verse
-for entry in ignore:
-    pages.remove(entry)
+
+pages = RootPage(request).getPageList(user='', include_underlay=False)
+
+
+#Quitamos las páginas de usuarios
+user_pages = []
+
+for uid in user.getUserList(request):
+    name = user.User(request, uid).name
+    if Page(request, name).exists():
+        user_pages.append(name)
+
+ignore += user_pages
+
+pages = set(pages) - set(ignore)
+
+#creo un diccionario pagina:{hijos}
+
+nodos = {}
+
+for page in pages:
+    nodos[page] = Page(request, page).getPageLinks(request)
+
+with open('node.dump', 'w') as fh:
+    pickle.dump(nodos, fh)
+
 
 #Iniciamos el Grafico
 graph = gv.AGraph(directed=True, splines="true", )
 
+#FIXME agregar progressbar
+
 print "Rellenando nodos"
 for page in pages:
     page_utf8 = page.encode('utf8')
-    graph.add_node(page_utf8, shape ='plaintext', href="http://python.org.ar/pyar/"+page_utf8, tooltip = "Click para ir a la página")
+    graph.add_node(page_utf8, 
+                    shape ='plaintext', 
+                    href="http://python.org.ar/pyar/"+page_utf8, 
+                    tooltip = "Click para ir a la página"
+                    )
     print '.',    
 for page in pages:
     links = Page(request, page).getPageLinks(request)
     page = page.encode("utf8")
     color = random.randint(0, 0xffffff)    
     for link in links:
-        if link in pages:
+        if link in pages and link != u'PyAr':
             link = link.encode("utf8")
             graph.add_edge(page, link, color=int2hex_color(color))
     print '.',
+
     
 print "Generando Grafico"
+
 graph.draw("grafo.svg", prog='fdp')
 
 
